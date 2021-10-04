007e24fa37eafa68c060c810b2400c6bba1600ae
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-05 03:15:54 +0400

    Lesson 2 > Task > 3 tekralnan reqem

---
+++ week-1/Lesson_2/uc_eyni_reqemin_olmasi/uc_eyni_reqemin_olmasi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace uc_eyni_reqemin_olmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------  Tekrarlan 3 reqem sayi   -------------------------");
+            while (true)
+            {
+
+
+                
+
+                Console.Write("Eded : ");
+                string eded = Console.ReadLine();
+                int ededyoxlama = Convert.ToInt32(eded);
+                if (ededyoxlama / 1000 > 0 && ededyoxlama / 10000 == 0)
+                {
+
+
+                    int d = 0;
+                    for (int i = 0; i < eded.Length; i++)
+                    {
+                        string[] eynilik = eded.Split(eded[i]);
+                        d = d + eynilik.Length;
+
+                    }
+
+                    int reqemsayi = 14;
+
+                    if (d == reqemsayi)
+                        Console.WriteLine(">>>>>>>>>> Tebrikler.Tekarlanan 3 reqem var <<<<<<<<<");
+                    else
+                        Console.WriteLine(">>>>>>>>>>>> Tekrarlan 3 reqem yoxdur <<<<<<<<<<<<<<<");
+
+
+                }
+                else
+                {
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
