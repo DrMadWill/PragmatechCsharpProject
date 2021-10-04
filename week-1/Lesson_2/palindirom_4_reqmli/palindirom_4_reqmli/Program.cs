9710945fd2332785287926ae681e9865d8d101d2
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-05 01:15:28 +0400

    Palindrom 4 reqemli ededin yoxlanmasi

---
+++ week-1/Lesson_2/palindirom_4_reqmli/palindirom_4_reqmli/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace palindirom_4_reqmli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>> Ededin palidromlu]u yoxlanmasi <<<<<<<<<<<<<<<<<<<<");
+
+            while (true)
+            {
+                
+
+                Console.Write("Eded : ");
+                string eded = Console.ReadLine();
+
+                int yoxlama = Convert.ToInt32(eded);
+
+                if (eded[0] == eded[3] && eded[1] == eded[2])
+                    Console.WriteLine("Tebrikler. Bu palidraom ededdir");
+                else
+                    Console.WriteLine("Palindrom deyil");
+
+                
+
+                Console.Write("Palidromu yoxlamaqa davam? >> klavaturan her hansi tusu /n :");
+                string davamyoxlama=Console.ReadLine();
+                if (davamyoxlama == "n")
+                    break;
+
+        
+            }
+        }
+    }
+}
