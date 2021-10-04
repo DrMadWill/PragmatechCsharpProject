7439d6a4f6f522cf78d6e25f30a9aa39a5423dee
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-05 02:45:57 +0400

    Lesson 2 > Task > Tekrarlan eded tap

---
+++ week-1/Lesson_2/eyni_reqem_yoxlmasi/eyni_reqem_yoxlmasi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace eyni_reqem_yoxlmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("-------------------  Tekrarlan eded sayi  -------------------------");
+            while (true)
+            {
+
+
+                Console.Write("Reqemsayi : ");
+                int reqemsayi = Convert.ToInt32(Console.ReadLine()) * 2;
+
+                Console.Write("Eded : ");
+                string eded = Console.ReadLine();
+                int ededyoxlama = Convert.ToInt32(eded);
+                int d = 0;
+                for (int i = 0; i < eded.Length; i++)
+                {
+                    string[] eynilik = eded.Split(eded[i]);
+                    d = d + eynilik.Length;
+
+                }
+
+                if (d == reqemsayi)
+                    Console.WriteLine(">>>>>>>>>>>> Tekrarlan eded yoxdur <<<<<<<<<<<<<<<");
+                else
+                    Console.WriteLine(">>>>>>>>>> Tebrikler.Tekarlanan eded var <<<<<<<<<");
+
+
+
+            }
+
+        }
+    }
+}
