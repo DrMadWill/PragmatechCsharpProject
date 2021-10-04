92df92bd5f31e13b2ecf67ba241aaa3753b00062
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-05 02:10:16 +0400

    Lesson 2 >  Artan reqmli ededi

---
+++ week-1/Lesson_2/artansirlali_5_reqmli/artansirlali_5_reqmli/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace artansirlali_5_reqmli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededleri artan sirlari olmaqi");
+
+            while (true)
+            {
+                Console.Write("Nece reqemli ededdir? : ");
+                int emeliyat_sayi = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                Console.Write("Eded : ");
+                string eded = Console.ReadLine();
+                int ededyoxlama = Convert.ToInt32(eded);
+
+                int emeliyyat = 0;
+                for (int i = 0; i < eded.Length - 1; i++)
+                {
+                    if (Convert.ToInt32(eded[i]) < Convert.ToInt32(eded[i + 1]))
+                    {
+                        emeliyyat++;
+                    }
+                }
+
+                if (emeliyyat == emeliyat_sayi)
+                {
+                    Console.WriteLine(">>>>> Tebrikler.Ededler artan siralidir <<<<<<<<<<<<");
+                }
+                else
+                {
+                    Console.WriteLine(">>>>> Artan sirali deyil <<<<<<<<<<<<");
+                }
+
+            }
+        }
+    }
+}
