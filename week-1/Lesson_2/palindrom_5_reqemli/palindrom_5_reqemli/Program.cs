afccaf93f46a0b4c77248d2ef7ab070b9fda3236
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-05 01:28:39 +0400

    Lesson 2 Palindrom 5 reqqemli eded

---
+++ week-1/Lesson_2/palindrom_5_reqemli/palindrom_5_reqemli/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace palindrom_5_reqemli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>> Ededin palidromlu]u yoxlanmasi <<<<<<<<<<<<<<<<<<<<");
+
+            while (true)
+            {
+
+
+                Console.Write("Eded : ");
+                string eded = Console.ReadLine();
+
+                int yoxlamaeded = Convert.ToInt32(eded);
+
+                if (eded[0] == eded[4] && eded[1] == eded[3])
+                    Console.WriteLine("----->Tebrikler. Bu palidraom ededdir");
+                else
+                    Console.WriteLine("----->Palindrom deyil");
+
+
+
+                Console.Write("Palidromu yoxlamaqa davam? >> klavaturan her hansi tusu /n :");
+                string davamyoxlama = Console.ReadLine();
+                if (davamyoxlama == "n")
+                    break;
+
+
+            }
+        }
+    }
+}
