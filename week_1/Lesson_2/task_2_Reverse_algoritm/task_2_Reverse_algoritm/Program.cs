573df800e30547b170269bac39eceafe1b0acf50
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-18 11:04:17 +0400

    Week_1 > Lesson_2 > task _2

---
+++ week_1/Lesson_2/task_2_Reverse_algoritm/task_2_Reverse_algoritm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace task_2_Reverse_algoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Text : ");
+            string texts = Console.ReadLine();
+             
+            string outline;
+            int texts_length = texts.Length - 1;
+
+            for (int i = 0; i < texts.Length; i++)
+            {
+                outline =Convert.ToString(texts[texts_length - i]);
+                Console.Write(outline);
+                
+            }
+
+            
+            
+            
+        }
+    }
+}
