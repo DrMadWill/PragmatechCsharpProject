97723c11935ff9e0dfc6a7ba0f5ec34b32a859b6
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-22 16:12:30 +0400

    Week_2 > Lesson_1 > Lesson >

--- week_1/Lesson_2/task_3/task_3/Program.cs
+++ week_1/Lesson_2/task_3/task_3/Program.cs
@@ -9,52 +9,44 @@ namespace task_3
             Console.WriteLine("Hello World!");
             Console.Write("Text : ");
             string text = Console.ReadLine();
-            int stop = (text.Length-1) / 2;
+            int stop = (text.Length - 1) / 2;
+            int eded1 = text.Length / 2;
+            float eded = (text.Length) / 2f;
+
+            float yokla = eded - (float)eded1 ;
+            Console.WriteLine(">>>> yokla >>>"+yokla);
 
 
-
-            
             int bc = 0;
             for (int i = 0; i <= stop; i++)
             {
 
+                
 
                 if (text[i] == text[text.Length - 1 - i])
                 {
 
                     bc += 1;
-                    Console.WriteLine(bc);
+                    Console.WriteLine(">>>>> if >>>"+bc);
+                }
+                if (yokla > 0 && i == stop)
+                {
+                    bc = bc - 1;
                 }
 
 
             }
 
-            if (text.Length/2 == bc)
+            if ((text.Length -1)/ 2 == bc)
             {
-                Console.WriteLine("Good boy");
+                Console.WriteLine(">>>>> Palindromdur");
             }
             else
             {
-                Console.WriteLine("Bad boy");
+                Console.WriteLine(">>> Palimdrom deyil");
             }
 
 
-            //string[] sad = new string[] { "salam ", "necesen" };
-            //string resault="";
-            //foreach (string elemet in sad)
-            //{
-            //    resault += elemet;
-            //}
-
-
-
-
-            ////char a = 'a';
-            ////char b = 'c';
-            ////resault = Convert.ToString(a) + Convert.ToString(b);
-
-
-            //Console.WriteLine(resault);
 
 
 
---
+++ week_2/Lesson_1/Lesson/Lesson/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            #region Params
+            //getControlPrams(1,2,3,4);
+
+            //int[] variable_int = { 1, 5, 5 };
+
+            //getArray(variable_int);
+
+            #endregion
+
+
+            GameUser user_1 = new GameUser("MadWill",255);
+            user_1.Race = "Learman";
+
+            Console.WriteLine(user_1.UserName);
+
+
+        }
+
+        #region Params Method
+        //public static void getControlPrams(params int[] list)
+        //{
+        //    foreach (var elemnt in list)
+        //    {
+        //        Console.WriteLine(elemnt);
+        //    }
+        //}
+
+        //public static void getArray( int[] list)
+        //{
+        //    foreach (var elemnt in list)
+        //    {
+        //        Console.WriteLine(elemnt);
+        //    }
+        //}
+        #endregion
+
+    }
+
+    class GameUser
+    {
+        public string UserName;
+        public int Level;
+        public string Race; 
+
+        public GameUser(string user,int level)
+        {
+            UserName = user;
+            Level = level;
+        }
+
+
+    }
+
+
+
+
+
+}
