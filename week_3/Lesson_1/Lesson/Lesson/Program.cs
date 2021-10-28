8896f57ebbd1a8f1801852b01a7da37ba9a62776
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-28 22:24:21 +0400

    Week_2 > Lesson_2 > Lesson > Non generic type

--- week_3/Lesson_1/Lesson/Lesson/Program.cs
+++ week_3/Lesson_1/Lesson/Lesson/Program.cs
@@ -12,12 +12,13 @@ namespace Lesson
             #region Non Generic Type
             Console.WriteLine("Non Generic Type");
 
-            Hashtable ht = new Hashtable();
-            ht.Add("Key", "Value");
-            ht.Add("Naruto", "Hinata");
-            ht.Add("Yin", "Yang");
-            ht.Add(1, 2);
-            ht.Add(2, 4);
+            #region Hashtable
+            //hashtable ht = new hashtable();
+            //ht.add("key", "value");
+            //ht.add("naruto", "hinata");
+            //ht.add("yin", "yang");
+            //ht.add(1, 2);
+            //ht.add(2, 4);
             
             
 
@@ -34,11 +35,40 @@ namespace Lesson
 
             
 
-            foreach (var k in ht.Keys)
+            //foreach (var k in ht.Keys)
+            //{
+            //    Console.WriteLine(ht[k]);
+            //}
+            #endregion
+
+            #region Stack
+
+            //Stack st_type = new Stack();
+            //st_type.Push("Mad");
+            //st_type.Push(25);
+            //st_type.Push("will");
+
+            //foreach (var item in st_type)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region Queue
+
+            Queue my_duck = new Queue();
+            my_duck.Enqueue(25);
+            my_duck.Enqueue("SatAgenty");
+            my_duck.Enqueue("Can fly");
+
+
+            foreach (var item in my_duck)
             {
-                Console.WriteLine(ht[k]);
+                Console.WriteLine(item);
             }
 
+            #endregion
 
 
             #endregion
