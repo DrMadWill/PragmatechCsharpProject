9f492b06b26f93980935049dd220b9278b76e215
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-29 16:42:22 +0400

    Week_2 > Lesson_3 > Task

---
+++ week_3/Lesson_1/task_2/task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My World!");
+
+            Queue<string> que = new Queue<string>();
+            que.Enqueue("I");
+            que.Enqueue("Love");
+            que.Enqueue("duck");
+            que.Enqueue("but");
+            que.Enqueue("What");
+            que.Enqueue("is");
+            que.Enqueue("Queue");
+            que.Enqueue("1");
+            que.Enqueue("2");
+            que.Enqueue("3");
+            que.Enqueue("4");
+            que.Enqueue("5");
+            que.Enqueue("6");
+
+
+
+
+            int counts=0;
+            foreach (var item in que)
+            {
+                counts += 1;
+                if (counts > 10)
+                {
+                    if (counts == que.Count)
+                    {
+                        item
+                    }
+                }
+                Console.WriteLine(">>> " + item);
+            }
+
+        }
+    }
+}
---
+++ week_3/Lesson_1/task_3/task_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My World!");
+
+            List<int> numb = new List<int>();
+            int add;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("<<<<<<<<<<<< : ");
+                add = Convert.ToInt32(Console.ReadLine());
+                numb.Add(add);
+
+            }
+
+
+            List<int> first_5 = new List<int>();
+            List<int> last_5 = new List<int>();
+            for (int i = 0; i < numb.Count; i++)
+            {
+                if (i < 5)
+                {
+                    first_5.Add(numb[i]);
+                }
+                else
+                {
+                    last_5.Add(numb[i]);
+                }
+            }
+
+            List<int> newlist = new List<int>();
+            newlist.AddRange(last_5);
+            newlist.AddRange(first_5);
+            Console.WriteLine("******************************************");
+            foreach (var item in newlist)
+            {
+                Console.WriteLine(">>>>>>>>>>>>>"+item);
+            }
+
+
+        }
+     }
+}
