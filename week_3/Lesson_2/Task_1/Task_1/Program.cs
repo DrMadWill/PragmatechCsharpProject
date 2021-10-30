cc40785a8acc003840eefa7eb320b8a688b6d787
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-10-30 21:45:00 +0400

    Week_2 > Lesson_2 > Task_1 > add

---
+++ week_3/Lesson_2/Task_1/Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+
+
+    class Student
+    {
+        public static int Count;
+        public int No;
+        public string Name;
+        public string SureName;
+        
+        public Student(string name,string surename)
+        {
+
+            this.Name = name;
+            this.SureName = surename;
+            Count++;
+            this.No = Count;
+
+        }
+
+    }
+}
