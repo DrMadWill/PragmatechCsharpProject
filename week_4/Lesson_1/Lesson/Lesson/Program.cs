411a00500ae5b12ed9c8fa8351d3e69ca4494b1f
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-04 23:10:24 +0400

    Week_4 > Lesson_1 > Lesson Code => OOP Inheritance > UnCoplate

---
+++ week_4/Lesson_1/Lesson/Lesson/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My World!");
+
+            MYWorld MadWill = new MYWorld();
+            MadWill.Eks_varliqlar();
+
+            //World add = new World();
+
+
+        }
+    }
+
+
+
+
+    abstract class World
+    {
+        public string Container;
+        public void Eks_varliqlar()
+        {
+            Console.WriteLine("Bir birine qarsi mubarize");
+        }
+
+    }
+
+    class MYWorld : World
+    {
+
+
+        public void Learn()
+        {
+            Console.WriteLine("Gercekliyi oyren ,sebebleri oyern ,meqsedi oyren oyren oyer ..........");
+        }
+    }
+
+
+}
