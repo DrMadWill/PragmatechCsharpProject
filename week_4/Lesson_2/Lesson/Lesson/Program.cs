b4554afe7b01ca1ba04b496a4f5cdb9d77e6723e
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-07 22:14:38 +0400

    Week_4 > Lesson_2 > Struct and Class different

---
+++ week_4/Lesson_2/Lesson/Lesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My World!");
+            Console.WriteLine("-------- Struct and Class -------------------");
+
+            MW tipic = new MW();
+            tipic.Ideal = "CWorld";
+            Console.WriteLine(tipic.Ideal);
+
+        }
+    }
+
+    struct MW
+    {
+        public string Ideal;
+        string Life;
+
+    }
+}
