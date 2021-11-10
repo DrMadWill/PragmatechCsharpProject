892ad5e182cf1dd8c63c35565a13e45a0c3e2e69
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-10 22:59:44 +0400

    Week_5 > Lesson_1 > Interface > add

--- week_5/Lesson_1/Lesson/TestinInternal/Program.cs
+++ week_5/Lesson_1/Lesson/TestinInternal/Program.cs
@@ -16,4 +16,88 @@ namespace TestinInternal
     {
 
     }
+
+    abstract class Site_Operation
+    {
+        public abstract void Create();
+        public abstract void Read();
+        public abstract void Update();
+        public abstract void Delete();
+
+
+    }
+
+
+    class Allen : Site_Operation
+    {
+        public override void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface ISite_Opertion_CR
+    {
+        void Create();
+        void Read();
+
+    }
+
+    interface ISite_Opation_UD
+    {
+        void Update();
+        void Delete();
+    }
+
+    class MadWill : ISite_Opertion_CR,ISite_Opation_UD
+    {
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Novel : ISite_Opertion_CR
+    {
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
