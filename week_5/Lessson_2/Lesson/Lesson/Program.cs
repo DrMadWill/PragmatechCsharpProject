60571df4bd3e47853d8827438d65e6d63c538e94
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-14 17:42:24 +0400

    Week_5 > Lesson_2 > Lesson  > add

---
+++ week_5/Lessson_2/Lesson/Lesson/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My World!");
+
+            ICrudForAdmin maid = new User();
+            maid.Update();
+
+
+        }
+    }
+
+
+    interface ICrudForAdmin
+    {
+        public void Create()
+        {
+            Console.WriteLine("I am Created");
+        }
+
+        void Update();
+        
+
+    }
+
+    interface ICrudForClent
+    {
+        void Read();
+    }
+
+
+    class User : ICrudForClent,ICrudForAdmin
+    {
+        public void Read()
+        {
+            Console.WriteLine("User Read");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("User Update");
+        }
+    }
+
+
+
+
+
+
+}
