a98e2989d77cad1acfc30c07b42365555f6bead9
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-15 21:37:14 +0400

    Hacker Rank > Mini_MaxSum > add

---
+++ HackerRank/Mini_MaxSum/Mini_MaxSum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Mini_MaxSum
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            arr.Sort();
+            int a = 0;
+            for (int i = 0; i < arr.Count-1; i++)
+            {
+                a += arr[i];
+            }
+
+            int b = 0;
+            for (int i = 1; i < arr.Count; i++)
+            {
+                b += arr[i];
+            }
+
+            Console.WriteLine(a+" "+b);
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+        }
+    }
+
+}
