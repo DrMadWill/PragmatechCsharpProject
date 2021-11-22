3507b2a08b4aba29e8982814b5fc101bb20d97b7
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-22 23:54:24 +0400

    week_6 > Lesson_2 > Task > fix up

--- week_6/Lesson_1/Task/Task/Program.cs
+++ week_6/Lesson_1/Task/Task/Program.cs
@@ -19,6 +19,48 @@ namespace Task
             tarkim.AddMedicine(medicine2);
             tarkim.AddMedicine(medicine3);
             tarkim.AddMedicine(medicine4);
+            int price;
+            int count;
+            string name;
+            bool is_continue_last;
+            bool is_continue_create_m;
+            do
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    do
+                    {
+                        Console.WriteLine(" >>>>> Medicine Create <<<<<");
+                        Console.WriteLine(" <<<<<<<<<< Input Medicine Name ");
+                        name = My.ConsInputString();
+                        Console.WriteLine(" <<<<<<<<<< Input Medicine Price ");
+                        price = My.ConsInputInt();
+                        Console.WriteLine(" <<<<<<<<<< Input Medicine Count ");
+                        count = My.ConsInputInt();
+                        try
+                        {
+                            Medicine medicine = new Medicine(name, price, count);
+                            tarkim.AddMedicine(medicine);
+                            is_continue_create_m = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            Console.WriteLine("Can't create medicine! Plase input agen .");
+                            is_continue_create_m = false;
+                            
+                        }
+
+                    } while (is_continue_create_m);
+                    
+                    
+                }
+                is_continue_last = My.IsContinueLoop();
+
+            } while (is_continue_last);
+            
+
+             
 
             //tarkim.ShowInfoList();
             //Medicine medicine2 = new Medicine("Fosetrin", 21, 12);//
@@ -42,24 +84,24 @@ namespace Task
             //medicine4.ShowInfo();
             //Console.WriteLine(medicine4.TotalIncome);
 
-            medicine4.ShowInfo();
-            tarkim.Sell("nemroot", 10);
-            medicine4.ShowInfo();
-            Console.WriteLine(medicine4.Name + " Total Came >> "+medicine4.TotalIncome);
+            //medicine4.ShowInfo();
+            //tarkim.Sell("nemroot", 10);
+            //medicine4.ShowInfo();
+            //Console.WriteLine(medicine4.Name + " Total Came >> "+medicine4.TotalIncome);
 
 
-            medicine3.ShowInfo();
-            tarkim.Sell("Pririnos-4", 10);
-            medicine3.ShowInfo();
-            Console.WriteLine(medicine3.Name +" Total Came >> " + medicine3.TotalIncome);
-            Console.WriteLine("Common Totalincame : " + tarkim.TotalCome);
+            //medicine3.ShowInfo();
+            //tarkim.Sell("Pririnos-4", 10);
+            //medicine3.ShowInfo();
+            //Console.WriteLine(medicine3.Name +" Total Came >> " + medicine3.TotalIncome);
+            //Console.WriteLine("Common Totalincame : " + tarkim.TotalCome);
 
-            medicine4.ShowInfo();
-            tarkim.Sell("nemroot", 2);
-            medicine4.ShowInfo();
-            Console.WriteLine(medicine4.Name + " Total Came >> " + medicine4.TotalIncome);
+            //medicine4.ShowInfo();
+            //tarkim.Sell("nemroot", 2);
+            //medicine4.ShowInfo();
+            //Console.WriteLine(medicine4.Name + " Total Came >> " + medicine4.TotalIncome);
 
-            Console.WriteLine("Common Totalincame : " + tarkim.TotalCome);
+            //Console.WriteLine("Common Totalincame : " + tarkim.TotalCome);
 
         }
     }
