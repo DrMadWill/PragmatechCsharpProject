1f162e5e90644d51660667ab32c9411a4bc5c7d4
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-28 14:02:19 +0400

    Week_7 > Lesson_6 > Lesson > Event >add

--- week_7/Lesson_2/Lesson/Lesson/Program.cs
+++ week_7/Lesson_2/Lesson/Lesson/Program.cs
@@ -22,20 +22,48 @@ namespace Lesson
             //Cons += Cwrt;
             //Cons.Invoke();
 
-            C = Cwrite;
-            C += Program_C;
-            C += Cwrt;
-            C();
 
+
+            //C = Cwrite;
+            //C += Program_C;
+            //C += Cwrt;
+            //C();
+
+
+            MVC mVC = new MVC(500);
+            // Atm
+            mVC.BalanceFinisid += (a, b) =>
+            {
+                My.NoteOutput($"Balansda {b} qederdir. Istenir {a}");
+            };
+            //User
+            mVC.BalanceFinisid += UserBalanceFinshed;
+
+            mVC.TransferSuccessfly += (z, y, time) =>
+            {
+                My.NoteOutput($"{time} vaxta {z} qeder mebleq balasdan cixarildi. qaliq {y}");
+            };
+
+            mVC.WithDraw(100);
+            mVC.WithDraw(150);
+            mVC.WithDraw(260);
 
         }
 
+
+        public static void UserBalanceFinshed(double a, double b)
+        {
+            My.NoteOutput($"hazirda balancda {a} qeder mebleq yoxdur . Balanc {b} AZN");
+        }
+
+
+        #region My Research
         private static void Program_C()
         {
             My.NoteOutput("Not Found ");
         }
 
-        static event Cws C;
+        
 
         static void Cwrite()
         {
@@ -49,8 +77,61 @@ namespace Lesson
 
         }
 
+        public static event Cws C;
 
+        #endregion
     }
+
+
+    class MVC
+    {
+        public event Action<double, double> BalanceFinisid;
+        public event Action<double, double, DateTime> TransferSuccessfly;
+
+
+        private double _balance;
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+
+            set
+            {
+                _balance = value;
+            }
+
+        }
+
+
+        public MVC(double balance)
+        {
+            Balance = balance;
+        }
+
+
+        public void WithDraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                if (TransferSuccessfly != null)
+                {
+                    TransferSuccessfly.Invoke(amount, Balance, DateTime.Now);
+                }
+                return;
+            }
+            else
+            {
+                if (BalanceFinisid != null)
+                {
+                    BalanceFinisid.Invoke(amount, Balance);
+                }
+            }
+        }
+
+    }   
 
     public delegate void Cws();
 
