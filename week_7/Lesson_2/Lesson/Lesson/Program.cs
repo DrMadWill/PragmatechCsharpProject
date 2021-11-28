b3db5ed9c69c0fe2d291ffa1cdf37a5c0eda8f3d
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-11-28 11:48:40 +0400

    week_7 > lesson_2 > Lesson > add > Delegate

---
+++ week_7/Lesson_2/Lesson/Lesson/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lesson
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Delegate New Using
+
+            //Cws Cons = new Cws(Cwrite);
+            //Cons += Cwrt;
+            //Cons -= Cwrite;
+
+            //Cons.Invoke();
+
+            #endregion
+
+
+            //Action Cons = Cwrite;
+            //Cons += Cwrt;
+            //Cons.Invoke();
+
+            C = Cwrite;
+            C += Program_C;
+            C += Cwrt;
+            C();
+
+
+        }
+
+        private static void Program_C()
+        {
+            My.NoteOutput("Not Found ");
+        }
+
+        static event Cws C;
+
+        static void Cwrite()
+        {
+            My.NoteHead("Hello My WWorld");
+        }
+
+
+        static void Cwrt()
+        {
+            My.NoteHead("Hello I");
+
+        }
+
+
+    }
+
+    public delegate void Cws();
+
+}
