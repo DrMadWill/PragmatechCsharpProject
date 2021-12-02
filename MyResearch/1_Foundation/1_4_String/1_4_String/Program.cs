24aec19052bd82cfce9ff09d6ab8cc89f63cb4ef
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-12-02 14:49:04 +0400

    My Research > 1_Foundation > 1_4 String > new add

--- MyResearch/1_Foundation/1_4_String/1_4_String/Program.cs
+++ MyResearch/1_Foundation/1_4_String/1_4_String/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Text;
 namespace _1_4_String
 {
     class Program
@@ -15,14 +15,24 @@ namespace _1_4_String
             //string str = string.Format("My name is {0} {1} and I'am {2} years old.",first_name,last_name,age);
             string str =$"My name is {first_name} {last_name} and I'am {age} years old." ;
 
-            string[] texts = new string[] { "My", " name ", " is ",first_name,last_name };
-            string resault = "";
+            string[] texts = new string[] { "My", " name ", " is ",first_name," ",last_name };
 
+            string resault_string = "";
+            
+            // yaddaş prablemei
             for (int i = 0; i < texts.Length; i++)
             {
-                resault += texts[i];
+                resault_string += texts[i];
             }
-            Console.WriteLine(resault);
+            Console.WriteLine(resault_string);
+
+            // yaddaş prablemei
+            StringBuilder result_stringBilder = new StringBuilder();
+            for (int i = 0; i < texts.Length; i++)
+            {
+                result_stringBilder.Append(texts[i]);
+            }
+            Console.WriteLine(result_stringBilder);
 
 
             Console.WriteLine(str);
