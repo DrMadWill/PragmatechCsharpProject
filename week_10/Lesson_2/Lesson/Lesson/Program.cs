c346c3b11bdbb64bb5c7a7cf541dda6626fcc840
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-12-19 12:08:44 +0400

    week_10 > Lesson_2 > lesson > Reflection + Async Documantation > add

---
+++ week_10/Lesson_2/Lesson/Lesson/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Student st = new Student("Statick");
+
+            Console.WriteLine(st.GetType().Assembly);
+            foreach (var item in st.GetType().GetMethods())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name;
+        public bool isAdele;
+        public static int total;
+        public void GetWirte()
+        {
+            Console.WriteLine("Hi,You big man ");
+        }
+
+        public Student(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+}
---
+++ week_10/Readme.md
@@ -0,0 +1,4 @@
+
+## Async and Await
+> Məlumatlar ardıcıl icrası zamanı `return` type olan methodlar bir cavab gözlədiyi üçün həmin methodun icrası bitənə qədər növbəti method gözlədilir. Nəticə etibarı ilə bizim böyük problemiz olur bunu qaşısını almaq üçün  **asinxron sistem**-dən istifadə edilir. Nədir bu asinxron ? Yəni method bir nəticə gözləyirsə və ya methodun arxada icra olunacaq uzun funksiyası varsa və s.  kimi hallara baxmayaraq bütün methodlar gəlib icra tələblərni göndərir sonra hansı methodun icrası başa çatıbsa həmin method nəticəni götürüb aralaşır.
+- Fail oxumaq üçün StreamRead classında istifadə edilir və bu `using`  içərisində yazılır.
