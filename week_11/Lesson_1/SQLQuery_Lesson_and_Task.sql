73a8ea77146e2a9ace22ca934cc232ca98c92edd
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-12-24 16:45:42 +0400

    week_11 > lesson_1 >  Task and Lesson > SQL > add

---
+++ week_11/Lesson_1/SQLQuery_Lesson_and_Task.sql
@@ -0,0 +1,52 @@
+-- First Query
+
+-- Create database
+--create database CSharp
+
+
+-- Create Tabel
+--create table Student(
+--Id int identity(1,1),
+--FirstName varchar(25),
+--LastName varchar(35),
+--Email varchar(60),
+--BirthDate date,
+--Practice decimal
+--)
+
+ --Insert Tabel
+--Insert into dbo.Student(FirstName,LastName,Email,BirthDate,Practice)
+--values
+--('Hemid','Ibadov','hemidov@gmail.com','1990-10-1',5),
+--('Sadiq','Elesgerov','elesgerov@gmail.com','1998-09-07',5),
+--('Samir','Ehmedov','sadiqov@gmail.com','1800-08-06',7)
+
+-- Select Information
+--select * from dbo.Student
+--select * from dbo.Student where FirstName='Will'
+
+-- Delete Inforamation
+--delete from dbo.Student where Id=11
+--delete from dbo.Student where Id between 9 and 10
+
+
+-- Select Information Other Way
+--select * from dbo.Student
+--select * from dbo.Student where LastName like '%v'
+--select * from dbo.Student where LastName like 'e%'
+--select * from dbo.Student where LastName like '%ad%'
+--select TOP(3) * from dbo.Student
+--select * from dbo.Student order by id offset 6 rows
+
+-- Update Information
+--Update dbo.Student set FirstName ='MadWill',LastName='DomationRoad' where Id=1 
+
+
+
+-- Update tabel
+--Alter table dbo.Student add Ideal varchar(150)
+
+
+-- Delete Colomn
+--Alter table dbo.Student drop column Ideal 
+
