2a2490e4f047a0883ac3acfd14e4ca1eb01314da
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2021-12-25 22:24:30 +0400

    week_11 > lesson_1 >  Tast > SQL > add

---
+++ week_11/Lesson_1/SQLQuery1.sql
@@ -0,0 +1,10 @@
+--CREATE DATABASE Groups
+--Create table Group655a2(
+--Id int primary key identity(1,1),
+--[Name] nvarchar(40) not null,
+--SureName nvarchar(40) not null,
+--Email varchar(60),
+--is_Practice Bit
+--);
+
+crea
