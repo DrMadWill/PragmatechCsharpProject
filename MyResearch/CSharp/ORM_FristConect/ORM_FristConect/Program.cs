8746153afffea71e39615daff66e45f5b5f4520b
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2022-01-10 19:59:15 +0400

    My Research > CSharp > ORM > First Connect

---
+++ MyResearch/CSharp/ORM_FristConect/ORM_FristConect/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace ORM_FristConect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //GetConnect();
+            GetMySQLConnect();
+
+        }
+
+        static void GetConnect()
+        {
+            string connectionString = @"Data Source = .\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security = SSPI;";
+          
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connected");
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+
+
+            }
+
+
+
+        }
+
+        static void GetMySQLConnect()
+        {
+            string connectionString = @"server=localhost;port=3306;database=drmadwill;user =root; password=nozh0506847748";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connected");
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+
+
+            }
+
+
+
+        }
+
+    }
+}
