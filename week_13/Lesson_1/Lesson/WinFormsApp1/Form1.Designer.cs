4758ed4fcc17637c5853ae5223b1ffd8f5fb2bec
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2022-01-11 11:38:42 +0400

    week_13 > Lesson_1 > Lesson > Ado.Net

---
+++ week_13/Lesson_1/Lesson/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combClass = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // combClass
+            // 
+            this.combClass.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.combClass.FormattingEnabled = true;
+            this.combClass.Location = new System.Drawing.Point(38, 58);
+            this.combClass.Name = "combClass";
+            this.combClass.Size = new System.Drawing.Size(213, 45);
+            this.combClass.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 603);
+            this.Controls.Add(this.combClass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combClass;
+    }
+}
+
---
+++ week_13/Lesson_1/Lesson/WinFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["WinFormsApp1.Properties.Settings.Setting"].ConnectionString;
+            //SqlConnection sqlConnection = null;
+            //SqlCommand sqlCommand = null;
+            //SqlDataReader sqlDataReader = null;
+
+            //try
+            //{
+
+            //    sqlConnection = new SqlConnection(connectionString);
+            //    sqlConnection.Open();
+
+            //    string command = "Select * from Groups";
+            //    sqlCommand = new SqlCommand(command, sqlConnection);
+
+            //    sqlDataReader = sqlCommand.ExecuteReader();
+
+            //    while (sqlDataReader.Read())
+            //    {
+            //        combClass.Items.Add(sqlDataReader[1]); 
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    throw;
+            //}
+            //finally
+            //{
+            //    sqlConnection.Close();
+            //}
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string command = "select * from Groups";
+                using (SqlCommand sqlCommand = new SqlCommand(command, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            combClass.Items.Add(sqlDataReader[1]);
+                        }
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
