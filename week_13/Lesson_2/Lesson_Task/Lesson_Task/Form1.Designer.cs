0b60232513e3daa434d5e159baffe7a00b82e675
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2022-01-13 02:46:24 +0400

    week_13 > Lesson_2 >Lesson_Task > Add > CRUD Form

---
+++ week_13/Lesson_2/Lesson_Task/Lesson_Task/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Lesson_Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textRull = new System.Windows.Forms.TextBox();
+            this.textStName = new System.Windows.Forms.TextBox();
+            this.textAge = new System.Windows.Forms.TextBox();
+            this.textCourse = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSeve = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(285, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rull Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(285, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(285, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(285, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Course";
+            // 
+            // textRull
+            // 
+            this.textRull.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textRull.Location = new System.Drawing.Point(550, 50);
+            this.textRull.Name = "textRull";
+            this.textRull.Size = new System.Drawing.Size(294, 38);
+            this.textRull.TabIndex = 4;
+            // 
+            // textStName
+            // 
+            this.textStName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textStName.Location = new System.Drawing.Point(550, 118);
+            this.textStName.Name = "textStName";
+            this.textStName.Size = new System.Drawing.Size(294, 38);
+            this.textStName.TabIndex = 5;
+            // 
+            // textAge
+            // 
+            this.textAge.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textAge.Location = new System.Drawing.Point(550, 208);
+            this.textAge.Name = "textAge";
+            this.textAge.Size = new System.Drawing.Size(294, 38);
+            this.textAge.TabIndex = 6;
+            // 
+            // textCourse
+            // 
+            this.textCourse.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textCourse.Location = new System.Drawing.Point(550, 295);
+            this.textCourse.Name = "textCourse";
+            this.textCourse.Size = new System.Drawing.Size(294, 38);
+            this.textCourse.TabIndex = 7;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(285, 393);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 50);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSeve
+            // 
+            this.btnSeve.Location = new System.Drawing.Point(450, 393);
+            this.btnSeve.Name = "btnSeve";
+            this.btnSeve.Size = new System.Drawing.Size(120, 50);
+            this.btnSeve.TabIndex = 9;
+            this.btnSeve.Text = "Save";
+            this.btnSeve.UseVisualStyleBackColor = true;
+            this.btnSeve.Click += new System.EventHandler(this.btnSeve_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(609, 393);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(120, 50);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(921, 393);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 50);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(762, 393);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(120, 50);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 600);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSeve);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.textCourse);
+            this.Controls.Add(this.textAge);
+            this.Controls.Add(this.textStName);
+            this.Controls.Add(this.textRull);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textRull;
+        private System.Windows.Forms.TextBox textStName;
+        private System.Windows.Forms.TextBox textAge;
+        private System.Windows.Forms.TextBox textCourse;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSeve;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ week_13/Lesson_2/Lesson_Task/Lesson_Task/Form1.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Lesson_Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textRull.Text = "";
+            textStName.Text = "";
+            textCourse.Text = "";
+            textAge.Text = "";
+        }
+
+        private void btnSeve_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Lesson_Task.Properties.Settings.Setting"].ConnectionString;
+            int rull = Convert.ToInt32(textRull.Text);
+            string stname = textStName.Text;
+            string coruse = textCourse.Text;
+            int age = Convert.ToInt32(textAge.Text);
+           
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string command = $"Insert into Person(Name,RollNumber,Age,Course)values('{stname}',{rull},{age},'{coruse}')";
+                using (SqlCommand sqlCommand = new SqlCommand(command, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    MessageBox.Show("Information added", "Added", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Lesson_Task.Properties.Settings.Setting"].ConnectionString;
+            int rull = Convert.ToInt32(textRull.Text);
+            string stname = textStName.Text;
+            string coruse = textCourse.Text;
+            int age = Convert.ToInt32(textAge.Text);
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string command = $"Update Person set Name='{stname}',Course='{coruse}',Age={age} where Person.RollNumber={rull}";
+                using (SqlCommand sqlCommand = new SqlCommand(command, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    MessageBox.Show("Information updated", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Lesson_Task.Properties.Settings.Setting"].ConnectionString;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string command = "select * from Person where Person.RollNumber=" + textRull.Text;
+                using (SqlCommand sqlCommand = new SqlCommand(command, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            textStName.Text = sqlDataReader[1].ToString();
+                            textAge.Text = sqlDataReader[3].ToString();
+                            textCourse.Text = sqlDataReader[4].ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Lesson_Task.Properties.Settings.Setting"].ConnectionString;
+            int rull = Convert.ToInt32(textRull.Text);
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string command = $"delete Person where RollNumber={rull}";
+                using (SqlCommand sqlCommand = new SqlCommand(command, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    MessageBox.Show("Information Deleted", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    
+                }
+            }
+        }
+    }
+}
