201450cf0cc150ef862e64ef7f9689003657adf9
DrMadWill <87474631+DrMadWill@users.noreply.github.com>
2022-01-14 12:36:16 +0400

    week_13 > Lesson_3 > Task > EF DB First > DataGridVeiw Add > Add

---
+++ week_13/Lesson_2/EFapp/EFapp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using EFapp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFapp
+{
+    public partial class Form1 : Form
+    {
+        private readonly AcademyEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new AcademyEntities();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = db.People.ToList<Person>();
+        }
+    }
+}
